acddefcdc305b4b1569595986380bb539f9290ee
塚崎成寛 <t.naruhiro.1026@gmail.com>
2021-06-10 23:36:42 +0900

    ref: vue-carousel-card

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,66 +1,70 @@
 <template>
   <div id="social">
-    <div class="d-flex justify-content-center flex-wrap">
-      <v-card class="mx-auto twitter-box">
-        <v-card-title>
-          <v-icon large top class="title twitter">
-            <i class="fab fa-twitter-square"></i> Twitter
-          </v-icon>
-        </v-card-title>
-        <div class='boad mt-4'>
-          <v-flex xs12>
-            <Timeline :id="user_id" sourceType="profile" :options="{ tweetLimit: '5' }"/>
-          </v-flex>
+    <CarouselCard :interval="7000" height="850px" type="card" arrow="hover">
+      <CarouselCardItem>
+        <div class="mx-auto twitter-box">
+          <span>
+            <div large top class="title twitter">
+              <i class="fab fa-twitter-square"></i> Twitter
+            </div>
+          </span>
+          <div class='boad mt-4'>
+            <Timeline :id="user_id" sourceType="profile" :options="{ tweetLimit: '5', width: '450' }"/>
+          </div>
         </div>
-      </v-card>
-      <v-card class="mx-auto ajax-box">
-        <v-card-title>
-          <v-icon large top class="title qiita">
-            <img src="@/assets/img/qiita_favicon.png" alt class="qiita_pic" /> Qiita
-          </v-icon>
-        </v-card-title>
-        <div class='boad mt-4'>
-          <a v-for="(result, i) in qiitas" v-bind:key="i" :href="result.url">
-            <b-card class="mx-auto pa-2 mb-3" style="max-width: 300px;" :title="result.title">
-              <b-badge variant="success" v-for="(tag, i) in result.tags" v-bind:key="i" class="mr-1">
-                {{tag.name}}
-              </b-badge>
-              <br>
-              <v-spacer></v-spacer>
-              <span>LGTM <b>{{result.likes_count }}</b></span><br>
-              <span>{{result.created_at | moment }}</span>
-            </b-card>
-          </a>
+      </CarouselCardItem>
+      <CarouselCardItem>
+        <div class="mx-auto ajax-box">
+          <span>
+            <div large top class="title qiita">
+              <img src="@/assets/img/qiita_favicon.png" alt class="qiita_pic" /> Qiita
+            </div>
+          </span>
+          <div class='boad mt-4'>
+            <a v-for="(result, i) in qiitas" v-bind:key="i" :href="result.url">
+              <b-card class="mx-auto pa-2 mb-3" style="max-width: 450px;" :title="result.title">
+                <b-badge variant="success" v-for="(tag, i) in result.tags" v-bind:key="i" class="mr-1">
+                  {{tag.name}}
+                </b-badge>
+                <br>
+                <v-spacer></v-spacer>
+                <span>LGTM <b>{{result.likes_count }}</b></span><br>
+                <span>{{result.created_at | moment }}</span>
+              </b-card>
+            </a>
+          </div>
         </div>
-      </v-card>
-      <v-card class="mx-auto ajax-box">
-        <v-card-title>
-          <v-icon large top class="title note">
-            <img src="@/assets/img/note_logo.png" alt class="qiita_pic" /> note
-          </v-icon>
-        </v-card-title>
-        <div class='boad mt-4'>
-          <a v-for="(note, i) in notes"
-              v-bind:key="i" :href="note.noteUrl">
-            <b-card
-              class="mx-auto pa-2 mb-3"
-              style="max-width: 300px;"
-              :title="note.name"
-              :img-src="note.eyecatch"
-              img-alt="Card image" img-bottom>
-              <b-badge variant="info" v-for="(tag, i) in note.hashtags" v-bind:key="i" class="mr-1">
-                {{tag.hashtag.name}}
-              </b-badge>
-              <br>
-              <v-spacer></v-spacer>
-              <span>スキ <b>{{note.likeCount }}</b></span><br>
-              <span>{{note.publishAt | moment }}</span>
-              <br>
-            </b-card>
-          </a>
+      </CarouselCardItem>
+      <CarouselCardItem>
+        <div class="mx-auto ajax-box">
+          <span>
+            <div large top class="title note">
+              <img src="@/assets/img/note_logo.png" alt class="qiita_pic" /> note
+            </div>
+          </span>
+          <div class='boad mt-4'>
+            <a v-for="(note, i) in notes"
+                v-bind:key="i" :href="note.noteUrl">
+              <b-card
+                class="mx-auto pa-2 mb-3"
+                style="max-width: 450px;"
+                :title="note.name"
+                :img-src="note.eyecatch"
+                img-alt="Card image" img-bottom>
+                <b-badge variant="info" v-for="(tag, i) in note.hashtags" v-bind:key="i" class="mr-1">
+                  {{tag.hashtag.name}}
+                </b-badge>
+                <br>
+                <v-spacer></v-spacer>
+                <span>スキ <b>{{note.likeCount }}</b></span><br>
+                <span>{{note.publishAt | moment }}</span>
+                <br>
+              </b-card>
+            </a>
+          </div>
         </div>
-      </v-card>
-    </div>
+      </CarouselCardItem>
+    </CarouselCard>
   </div>
 </template>
 
@@ -68,10 +72,14 @@
 import { Timeline } from 'vue-tweet-embed'
 import axios from 'axios'
 import moment from "moment"
+import { CarouselCard, CarouselCardItem } from 'vue-carousel-card'
+import 'vue-carousel-card/styles/index.css'
 
 export default {
   components: {
-    Timeline
+    Timeline,
+    CarouselCard,
+    CarouselCardItem
   },
   data: function() {
     return {
@@ -160,6 +168,32 @@ export default {
     display: inline-block;
     margin: 0 30px;
     vertical-align: middle;
+  }
+
+  h1 {
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF;
+    background: linear-gradient(90deg, rgba(156,247,121,1), rgba(59,183,12,1))
+  }
+
+  .carousel-card {
+    max-width: 80%;
+    margin: 0 auto;
+
+    &.carousel-card-container {
+      max-width: 80%;
+    }
+
+    // &.carousel-card-indicator {
+    //   &.is-active {
+    //     height: 120%;
+    //   }
+    // }
   }
 }
 </style>
